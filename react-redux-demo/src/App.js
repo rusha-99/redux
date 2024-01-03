9656185822058134e881574fa2bdfc6161e7e4db
rusha-99 <tharushapathirana07@gmail.com>
2024-01-03 10:17:19 +0530

    complete implementing react app of redux

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
+import {Provider} from "react-redux";
+import store from './redux/store'
 import CakeContainer from "./components/CakeContainer";
 
 
 function App() {
     return (
-        <div className="App">
-            <CakeContainer/>
-        </div>
+        <Provider store={store}>
+            <div className="App">
+                <CakeContainer/>
+            </div>
+        </Provider>
+
     );
 }
 
--- react-redux-demo/src/components/CakeContainer.js
+++ react-redux-demo/src/components/CakeContainer.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import {connect} from "react-redux";
+import {buyCake} from "../redux";
 
-function CakeContainer() {
+function CakeContainer(props) {
     return (
         <div>
-            <h2>Number of cakes</h2>
-            <button>Buy Cake</button>
+            <h2>Number of cakes - {props.numOfCakes}</h2>
+            <button onClick={props.buyCake}>Buy Cake</button>
         </div>
     )
 }
 
-export default CakeContainer
+const mapStateToProps = (state) => { //get redux state as a parameter
+    return {
+        numOfCakes: state.numOfCakes
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {//get dispatch method as a parameter
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)//connect react component to redux store
